0e9a74cc6d0f5a85c49d64be49365bb51e208252
Simon Gutwein <simon.bonvino@gmail.com>
2023-12-04 21:25:24 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,16 +5,12 @@ package_name = "CellPatchExtraction"
 def read_requirements():
     with open('requirements.txt', 'r') as file:
         return [line.strip() for line in file.readlines()]
-
-def get_version(package):
-    with open(f"./{package}/__init__.py") as file:
-        return [line.split("=")[-1].strip().strip("'") for line in file.readlines() if "__version__" in line][0]
     
 print(get_version(package_name))
 
 setup(
     name=package_name,
-    version=get_version(package_name),
+    version='{{VERSION_PLACEHOLDER}}',
     packages=find_packages(),
     install_requires=read_requirements(),
     author="Your Name",
